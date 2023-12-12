e67b425a5f6c831911c5c57515927305c937460e
Jean Arruda <135642337+jGuilhermexe@users.noreply.github.com>
2023-12-12 02:03:15 -0300

    teste

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!, tudo bem");
+        System.out.println("Hello, World! tudo e contigo estou otimo");
+
+    }
+}
